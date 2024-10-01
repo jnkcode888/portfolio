35e1712e8db60dd24bb15e13e120a15885ddf7ee
jnkcode888 <kinyuajohnnjoroge@gmail.com>
2024-10-01 16:02:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll effect for smooth navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
